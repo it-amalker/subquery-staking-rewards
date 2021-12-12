937a608afb01f71d7778e8553c389ec8fc57ea5b
amalker <it.amalker@yandex.ru>
2021-12-12 15:36:08 +0300

    4-th lesson

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -3,14 +3,14 @@ import {StakingReward, SumReward} from "../types";
 import {Balance} from "@polkadot/types/interfaces";
 
 export async function handleStakingRewarded(event: SubstrateEvent):Promise<void> {
-  const {event: {data: [account, newReward]}} = event;
-  const entity = new
-  StakingReward(`${event.block.block.header.number}-${event.idx.toString()
-  }`);
-  entity.account = account.toString();
-  entity.balance = (newReward as Balance).toBigInt();
-  entity.date = event.block.timestamp;
-  await entity.save();
+const {event: {data: [account, newReward]}} = event;
+const entity = new
+StakingReward(`${event.block.block.header.number}-${event.idx.toString()
+}`);
+entity.accountId = account.toString();
+entity.balance = (newReward as Balance).toBigInt();
+entity.date = event.block.timestamp;
+await entity.save();
 }
 
 function createSumReward(accountId: string): SumReward {
@@ -31,4 +31,10 @@ export async function handleSumRewarded(event: SubstrateEvent):Promise<void> {
   await entity.save();
 }
 
+export async function handleSumReward(event: SubstrateEvent):Promise<void> {
+  await handleSumRewarded(event)
+}
 
+export async function handleStakingReward(event: SubstrateEvent):Promise<void> {
+  await handleStakingRewarded(event)
+}
